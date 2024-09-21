c25f4150b5250f2618280501b7e34e986b7fd72e
Sahil Dinanath <sahildinanath@gmail.com>
2024-09-21 14:31:33 +0200

    changed starting port number

--- WitsFC-Codebase/start.sh
+++ WitsFC-Codebase/start.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 export OMP_NUM_THREADS=1
 
 host=${1:-localhost}
-port=${2:-3200}
+port=${2:-3100}
 
 for i in {1..11}; do
   python3 ./Run_Player.py -i $host -p $port -u $i -t WITS-FC -P 0 -D 0 &
