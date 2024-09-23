06eb11fc5b43a0eb37825e12332a76850f2a98f1
Sahil Dinanath <sahildinanath@gmail.com>
2024-09-23 21:10:28 +0200

    made test arrays use floats instead of ints

--- WitsFC-Codebase/strategy/Test_Submission_One.py
+++ WitsFC-Codebase/strategy/Test_Submission_One.py
@@ -5,8 +5,10 @@ from Submission_One import *
 class TestSubOne(unittest.TestCase):
     # has to use numpy array
     def test_subtract_row_minimum(self):
-        input = np.array([[15, 40, 45], [20, 60, 35], [20, 40, 25]])
-        expected_output = np.array([[0, 25, 30], [0, 40, 15], [0, 20, 5]])
+        input = np.array([[15.0, 40.0, 45.0], [20.0, 60.0, 35.0], [20.0, 40.0, 25.0]])
+        expected_output = np.array(
+            [[0.0, 25.0, 30.0], [0.0, 40.0, 15.0], [0.0, 20.0, 5.0]]
+        )
 
         test_matrix = input.copy()
 
@@ -15,8 +17,10 @@ class TestSubOne(unittest.TestCase):
         self.assertEqual(expected_output.tolist(), test_matrix.tolist())
 
     def test_subtract_col_minimum(self):
-        input = np.array([[0, 25, 30], [0, 40, 15], [0, 20, 5]])
-        expected_output = np.array([[0, 5, 25], [0, 20, 10], [0, 0, 0]])
+        input = np.array([[0.0, 25.0, 30.0], [0.0, 40.0, 15.0], [0.0, 20.0, 5.0]])
+        expected_output = np.array(
+            [[0.0, 5.0, 25.0], [0.0, 20.0, 10.0], [0.0, 0.0, 0.0]]
+        )
 
         test_matrix = input.copy()
         subtract_col_minimum(test_matrix)
@@ -24,7 +28,7 @@ class TestSubOne(unittest.TestCase):
         self.assertEqual(expected_output.tolist(), test_matrix.tolist())
 
     def test_cover_all_zeros_with_min_lines(self):
-        input = np.array([[0, 5, 25], [0, 20, 10], [0, 0, 0]])
+        input = np.array([[0.0, 5.0, 25.0], [0.0, 20.0, 10.0], [0.0, 0.0, 0.0]])
         test_matrix = input.copy()
         covered_rows = []
         covered_cols = []
@@ -37,8 +41,10 @@ class TestSubOne(unittest.TestCase):
         self.assertEqual(input.tolist(), test_matrix.tolist())
 
     def test_create_additional_zeros(self):
-        input = np.array([[0, 5, 25], [0, 20, 10], [0, 0, 0]])
-        expected_output = np.array([[0, 0, 20], [0, 15, 5], [5, 0, 0]])
+        input = np.array([[0.0, 5.0, 25.0], [0.0, 20.0, 10.0], [0.0, 0.0, 0.0]])
+        expected_output = np.array(
+            [[0.0, 0.0, 20.0], [0.0, 15.0, 5.0], [5.0, 0.0, 0.0]]
+        )
 
         test_matrix = input.copy()
         covered_rows = [2]
@@ -48,8 +54,10 @@ class TestSubOne(unittest.TestCase):
         self.assertEqual(expected_output.tolist(), test_matrix.tolist())
 
     def test_cover_zeros(self):
-        input = np.array([[15, 40, 45], [20, 60, 35], [20, 40, 25]])
-        expected_output = np.array([[0, 0, 20], [0, 15, 5], [5, 0, 0]])
+        input = np.array([[15.0, 40.0, 45.0], [20.0, 60.0, 35.0], [20.0, 40.0, 25.0]])
+        expected_output = np.array(
+            [[0.0, 0.0, 20.0], [0.0, 15.0, 5.0], [5.0, 0.0, 0.0]]
+        )
 
         test_matrix = input.copy()
         cover_zeros(test_matrix)
