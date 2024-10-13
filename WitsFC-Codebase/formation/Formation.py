06eddfb1448b1f80b09bd1b7dce3327bc416ef4c
Sahil Dinanath <sahildinanath@gmail.com>
2024-10-13 19:48:16 +0200

    added new formations for attacking, defending and midfield

--- WitsFC-Codebase/formation/Formation.py
+++ WitsFC-Codebase/formation/Formation.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# TODO: BIG we need to rearrange the positions to correlate to the player closest to said position
+
 
 def GenerateBasicFormation():
 
@@ -20,26 +22,75 @@ def GenerateBasicFormation():
     return formation
 
 
+# counter attack
 def four_four_two():
 
-    # stagger positions by 1 because of algorithm trying to find closest individual stuff
     formation = [
         np.array([-13, 0]),  # Goalkeeper
         #
-        np.array([-6, 7]),  # Left Defender
+        np.array([-7, 7]),  # Left Defender
         np.array([-7, -7]),  # Right Defender
         #
-        np.array([-9, 3]),  # Center Back Left
+        np.array([-10, 3]),  # Center Back Left
         np.array([-10, -3]),  # Center Back Right
         #
-        np.array([1, 3]),  # Center Midfielder Left
+        np.array([3, 3]),  # Center Midfielder Left
         np.array([3, -3]),  # Center Midfielder Right
         #
-        np.array([6, 7]),  # Left Midfielder
+        np.array([8, 7]),  # Left Midfielder
         np.array([8, -7]),  # Right Midfielder
         #
-        np.array([9, 4]),  # Forward Left
+        np.array([11, 4]),  # Forward Left
         np.array([11, -4]),  # Forward Right
+    ]
+
+    return formation
+
+
+# attack
+def three_four_three():
+
+    formation = [
+        np.array([-13, 0]),  # Goalkeeper
+        #
+        np.array([-7, 7]),  # Left Defender
+        np.array([-9, 0]),  # Center Back Left
+        np.array([-7, -7]),  # Right Defender
+        #
+        #
+        np.array([1, 3]),  # Center Midfielder Left
+        np.array([1, -3]),  # Center Midfielder Right
+        #
+        np.array([0, 5]),  # Left Midfielder
+        np.array([0, -5]),  # Right Midfielder
+        #
+        np.array([9, 4]),  # Forward Left
+        np.array([9, -4]),  # Forward Right
+        np.array([12, 0]),  # Center Back Right
+    ]
+
+    return formation
+
+
+# defend
+def five_four_one():
+    formation = [
+        np.array([-13, 0]),  # Goalkeeper
+        #
+        np.array([-8, 6]),  # Left Defender
+        np.array([-10, 0]),  # Center Back Left
+        np.array([-8, -6]),  # Right Defender
+        #
+        #
+        np.array([-9, 4]),  # Center Midfielder Left
+        np.array([-9, -4]),  # Center Midfielder Right
+        #
+        np.array([-4, 7]),  # Left Midfielder
+        np.array([-4, -7]),  # Right Midfielder
+        #
+        np.array([-3, 2]),  # Forward Left
+        np.array([-3, -2]),  # Forward Right
+        np.array([0, 0]),  # Center Back Right
     ]
 
     return formation
